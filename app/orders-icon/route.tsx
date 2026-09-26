9c852605ff4110217d9202a4d5cdbd44455aead0
8hz7fm29bb-jpg <8hz7fm29bb@privaterelay.appleid.com>
2026-09-26 14:53:03 +0200

    Remove black border from Orders Dock icon

--- app/orders-icon/route.tsx
+++ app/orders-icon/route.tsx
@@ -12,56 +12,44 @@ export async function GET() {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          background: '#171715',
+          background: '#ffd400',
+          color: '#21262c',
+          flexDirection: 'column',
         }}
       >
         <div
           style={{
-            width: 318,
-            height: 318,
-            borderRadius: 68,
-            background: '#f8d648',
             display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: '#21262c',
+            fontSize: 220,
+            lineHeight: 0.8,
+            fontWeight: 900,
+            letterSpacing: -12,
+            marginTop: -16,
           }}
         >
-          <div
-            style={{
-              display: 'flex',
-              fontSize: 170,
-              lineHeight: 0.82,
-              fontWeight: 900,
-              letterSpacing: -10,
-              marginTop: -2,
-            }}
-          >
-            22
-          </div>
-          <div
-            style={{
-              width: 220,
-              height: 13,
-              background: '#21262c',
-              display: 'flex',
-              marginTop: 24,
-            }}
-          />
-          <div
-            style={{
-              display: 'flex',
-              fontSize: 39,
-              lineHeight: 1,
-              fontWeight: 800,
-              letterSpacing: 12,
-              marginTop: 18,
-              marginLeft: 12,
-            }}
-          >
-            ORDERS
-          </div>
+          22
+        </div>
+        <div
+          style={{
+            width: 320,
+            height: 14,
+            background: '#21262c',
+            display: 'flex',
+            marginTop: 34,
+          }}
+        />
+        <div
+          style={{
+            display: 'flex',
+            fontSize: 48,
+            lineHeight: 1,
+            fontWeight: 800,
+            letterSpacing: 16,
+            marginTop: 24,
+            marginLeft: 16,
+          }}
+        >
+          ORDERS
         </div>
       </div>
     ),
